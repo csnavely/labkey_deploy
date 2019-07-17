e7765adaef30b1491fc8e55d393ae31477be552f
Edward Kirton <ESKirton@LBL.gov>
2019-07-17 13:10:15 -0700

    fix build.sh

--- metlims-nersc/app/build.sh
+++ metlims-nersc/app/build.sh
@@ -4,6 +4,6 @@ SPIN_USER="bpb"
 CONTAINER_NAME="metatlas_lims-app"
 VERSION=`date "+%Y-%m-%d-%H-%M"`
 echo "$VERSION"
-docker image build --tag "$CONTAINER_NAME-app:$VERSION" .
-docker image tag "$CONTAINER_NAME-app:$VERSION" "registry.spin.nersc.gov/$SPIN_USER/$NAME-app:$VERSION"
-docker image push "registry.spin.nersc.gov/$SPIN_USER/$CONTAINER_NAME-app:$VERSION
+docker image build --tag "$CONTAINER_NAME:$VERSION" .
+docker image tag "$CONTAINER_NAME:$VERSION" "registry.spin.nersc.gov/$SPIN_USER/$NAME:$VERSION"
+docker image push "registry.spin.nersc.gov/$SPIN_USER/$CONTAINER_NAME:$VERSION
